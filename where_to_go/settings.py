b29ad246304a34228392fccbb27638198cb04c4a
iugurchiev <isa.ugurchiev@gmail.com>
2022-06-02 12:31:05 +0300

    Fix static roots

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join('static'),)
 
-MEDIA_ROOT = os.path.join('media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
